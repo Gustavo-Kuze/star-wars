80098cecbee391a931c546b478e18bd5c7b0785e
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-05-18 14:36:34 -0300

    Criação do projeto

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# star-wars
+Entendendo o fluxo do Redux Saga com a API do Star Wars (swapi.co)
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
